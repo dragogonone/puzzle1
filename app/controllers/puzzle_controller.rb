ae369b5acbf547d96d54dc0a5e1f574158907b67
USER <dragogonone@gmail.com>
2016-03-17 15:48:57 +0900

    CSRF対策解除

--- app/controllers/puzzle_controller.rb
+++ app/controllers/puzzle_controller.rb
@@ -1,28 +1,31 @@
 class PuzzleController < ApplicationController
 
+protect_from_forgery except: [:index, :vote]
+
   def index
     if logged_in?
       @user = current_user
     else
       redirect_to root_url
     end
-  end
 
-  def vote
-    if params[:users][:target] == 'pikachu'
-      flash.now[:success] = 'ピカチュウに投票！'
-      current_user.update_attribute(:pikachu,current_user.pikachu+1)
-    elsif params[:users][:target] == 'coil'
-      flash.now[:success] = 'コイルに投票！'
-      current_user.update_attribute(:coil,current_user.coil+1)
-    else
-      render :text => '投票先がちがうよ'
-    end
     if current_user.coil > current_user.pikachu
       redirect_to controller: 'winners', action: 'new'
       #redirect_to :action => 'win'
     else
       render :action => 'index'
+    end
+
+  end
+
+  def vote
+  flash.now[:debug] = current_user.pikachu
+    if params[:target] == 'pikachu'
+      flash.now[:success] = 'ピカチュウに投票！'
+      current_user.update(pikachu: current_user.pikachu+1)
+    elsif params[:target] == 'coil'
+      flash.now[:success] = 'コイルに投票！'
+      current_user.update_attribute(:coil,current_user.coil+1)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'top#index'
   get  'login'   => 'sessions#new'
   post 'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  delete 'login'  => 'sessions#destroy'
   get 'signup' => 'users#new'
   post 'users' => 'users#create'
   get 'puzzle' => 'puzzle#index'
